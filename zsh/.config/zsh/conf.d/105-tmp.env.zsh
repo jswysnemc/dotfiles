baa2c83203a59e3f379221057592ce9f86931b5b
snemc <snemc@qq.com>
2025-09-15 00:49:13 +0800

    add: 添加变量 key 文件不存时的回退方案

--- zsh/.config/zsh/conf.d/105-tmp.env.zsh
+++ zsh/.config/zsh/conf.d/105-tmp.env.zsh
@@ -1,6 +1,24 @@
 _KEYS_DIR=$HOME/.ssh/keys
-export ANTHROPIC_BASE_URL=$(< $_KEYS_DIR/anthropic.url)
-export ANTHROPIC_API_KEY=$(< $_KEYS_DIR/anthropic.key)
+
+
+export_from_file() {
+    local var_name="$1"
+    local file_path="$2"
+    local fallback_value="${3:-""}"
+    local content="$fallback_value"
+
+    if [ -r "$file_path" ]; then
+        content=$(< "$file_path")
+    fi
+
+    declare -x "$var_name=$content"
+}
+
+
+
+export_from_file "ANTHROPIC_BASE_URL" "$_KEYS_DIR/anthropic.url"
+export_from_file "ANTHROPIC_API_KEY" "$_KEYS_DIR/anthropic.key"
+export_from_file "OPENROUTER_API_KEY" "/home/snemc/.ssh/keys/openrouter.key"
 export ANTHROPIC_AUTH_TOKEN=$ANTHROPIC_API_KEY
-export OPENROUTER_API_KEY=$(< /home/snemc/.ssh/keys/openrouter.key)
+
 # deepseek/deepseek-chat-v3.1:free
