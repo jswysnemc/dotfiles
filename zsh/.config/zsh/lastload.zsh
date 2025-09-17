944cd4c8082250b2a5527c5469b0cce19e4833b3
snemc <snemc@qq.com>
2025-09-17 23:41:58 +0800

    fix: 修复zsh使用的starship 在tty界面无法使用nerdfont，使用ps1/prompt变量回退

--- zsh/.config/zsh/lastload.zsh
+++ zsh/.config/zsh/lastload.zsh
@@ -1,1 +1,4 @@
-eval "$(starship init zsh)"
+# eval "$(starship init zsh)"
+
+
+[[ -t 1 && "$TERM" != "linux" ]] && eval "$(starship init zsh)" || PROMPT=$'%B%F{cyan}[%m@%n]%f%b %B%F{yellow}%d%f%b\n%B%F{green}%#%f%b '
