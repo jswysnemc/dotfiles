6028e6d0b600de5ab8c83efffa9020cc3459ea43
snemc <snemc@qq.com>
2025-09-19 02:44:10 +0800

    add: 添加pnpmhome

--- zsh/.config/zsh/conf.d/01-environment.zsh
+++ zsh/.config/zsh/conf.d/01-environment.zsh
@@ -1,3 +1,12 @@
 export PATH=$HOME/.custom/bin:$PATH
 export PATH=$PATH:$HOME/.local/share/cargo/bin
 
+# pnpm
+export PNPM_HOME="$HOME/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+
+
