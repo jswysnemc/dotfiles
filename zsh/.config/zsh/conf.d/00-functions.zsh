4187819ff41be085ae068d1479934ff49e7cee0c
snemc <snemc@qq.com>
2025-09-19 02:37:45 +0800

    add: 添加一个cv 函数， 用于清除文件内容后使用vim 编辑

--- zsh/.config/zsh/conf.d/00-functions.zsh
+++ zsh/.config/zsh/conf.d/00-functions.zsh
@@ -11,3 +11,23 @@ export_from_file() {
 
     declare -x "$var_name=$content"
 }
+
+
+# 清除文件内容并用编辑器打开
+cv() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: cv "
+    return 1
+  fi
+
+  # 检查文件是否存在，如果不存在则创建
+  if [[ ! -f "$1" ]]; then
+    touch "$1"
+  fi
+
+  # 清空文件内容
+  echo '' > "$1"
+
+  # 使用 $EDITOR 打开文件
+  ${EDITOR:-vim} "$1"
+}
