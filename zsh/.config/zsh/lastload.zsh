f6fed6eff7b322b8f2f3ce9eb186ac8b233dee13
snemc <snemc@qq.com>
2025-09-21 18:28:33 +0800

    fix： 对基础pormpt 添加 彩色

--- zsh/.config/zsh/lastload.zsh
+++ zsh/.config/zsh/lastload.zsh
@@ -1,4 +1,5 @@
 # eval "$(starship init zsh)"
 
 
-[[ -t 1 && "$TERM" != "linux" ]] && eval "$(starship init zsh)" || PROMPT=$'%B%F{cyan}[%m@%n]%f%b %B%F{yellow}%d%f%b\n%B%F{green}%#%f%b '
+[[ -t 1 && "$TERM" != "linux" ]] && eval "$(starship init zsh)" || PROMPT=$'%F{cyan}[%(!.%F{red}.%f)%n%F{cyan}@%m]%f %F{yellow}%~%f
+%F{green}%(!.#.$) %f'
